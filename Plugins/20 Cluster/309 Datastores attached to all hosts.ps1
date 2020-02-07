5e9c22d299701aefdd2ed821a1f2e320ad2d8039
Dario Doerflinger <dario@Darios-MBP.busit.ch>
2020-02-07 09:59:08 +0100

    Trying to fix multicluster behavior bug

--- Plugins/20 Cluster/309 Datastores attached to all hosts.ps1
+++ Plugins/20 Cluster/309 Datastores attached to all hosts.ps1
@@ -5,11 +5,11 @@ $DatastoresToIgnore = "*local*"
 
 foreach ($myCluster in $Clusters) {
     $allDatastores = $myCluster | Get-Datastore | where-object {$_.Name -notlike $DatastoresToIgnore} | Sort-Object Name
-    $allHosts = $myCluster | Get-VMHost | Where-Object {$_.ConnectionState -eq "Connected"}
+    $allHosts = $myCluster | Get-VMHost | Where-Object {$_.ConnectionState -eq "Connected"} | Sort-Object
     ForEach ($myHost in $allHosts) {
         $count++
         $report = New-Object PSObject
-        $report | Add-Member -MemberType NoteProperty -Name "ClusterName" -Value $myCluster.Name
+        #$report | Add-Member -MemberType NoteProperty -Name "ClusterName" -Value $myCluster.Name
         $report | Add-Member -MemberType NoteProperty -Name "HostName" -Value $myHost.Name
         ForEach ($myDatastore in $allDatastores) {
             $getDS = $myHost | Get-Datastore -Name $myDatastore.Name -ErrorAction SilentlyContinue
@@ -38,5 +38,8 @@ $Header = "[Cluster] Check datastore visibility per cluster"
 $Comments = "Checking if all hosts in a cluster can see all the same datastores"
 $Display = "Table"
 $Author = "Dario Doerflinger"
-$PluginVersion = 1.0
+$PluginVersion = 1.1
 $PluginCategory = "vSphere"
+
+# Change Log:
+# Removed Clustername. Issue: Mutli Cluster Environment not displayed correctly.
